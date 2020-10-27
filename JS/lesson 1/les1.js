ff6461eff077746ae0498b0dfb7d9d54bdbe81ce
Serhiy <vintonyak0601@gmail.com>
2020-10-28 01:49:01 +0200

    JS les11

---
+++ JS/lesson 1/les1.js
@@ -0,0 +1,34 @@
+// - присвоить каждому из следующих значений свою переменную:
+
+//   'hello',
+
+let sayHi = 'hallo';
+console.log(sayHi);
+alert(sayHi);
+document.write(sayHi);
+//   'owu',
+let school = 'owu';
+console.log(school);
+alert(school);
+document.write(school);
+
+//   'com',
+let domain = 'com';
+console.log(domain);
+alert(domain);
+document.write(domain);
+
+
+//   'ua',
+//   1,
+//   10,
+//   -999,
+//   123,
+//   3.14,
+//   2.7,
+//   16,
+//   true,
+//   false
+//
+//
+// Вывести каждую при помощи console.log , alert, document.write
