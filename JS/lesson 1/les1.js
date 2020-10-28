1b02d7656220a8eded97ea88f8f27713c5220cc0
Serhiy <vintonyak0601@gmail.com>
2020-10-28 02:19:21 +0200

    JS les new

--- JS/lesson 1/les1.js
+++ JS/lesson 1/les1.js
@@ -2,31 +2,71 @@
 
 //   'hello',
 
-let sayHi = 'hallo';
-console.log(sayHi);
-alert(sayHi);
-document.write(sayHi);
-//   'owu',
-let school = 'owu';
-console.log(school);
-alert(school);
-document.write(school);
-
-//   'com',
-let domain = 'com';
-console.log(domain);
-alert(domain);
-document.write(domain);
-
-
-//   'ua',
+// let sayHi = 'hallo';
+// console.log(sayHi);
+// alert(sayHi);
+// document.write(sayHi);
+// //   'owu',
+// let school = 'owu';
+// console.log(school);
+// alert(school);
+// document.write(school);
+//
+// //   'com',
+// let domain = 'com';
+// console.log(domain);
+// alert(domain);
+// document.write(domain);
+//
+// //   'ua',
+// let codeUkraine = 'ua';
+// console.log(codeUkraine);
+// alert(codeUkraine);
+// document.write(codeUkraine);
 //   1,
+let number = 1;
+console.log(number);
+alert(number);
+document.write(number);
+
 //   10,
+let ocWindows = 10;
+console.log(ocWindows);
+alert(ocWindows);
+document.write(ocWindows);
+
 //   -999,
+let musicalGroup = -999;
+console.log(musicalGroup);
+alert(musicalGroup);
+document.write(musicalGroup);
+
+
 //   123,
+
+let randomNumber = 123;
+console.log(randomNumber);
+alert(randomNumber);
+document.write(randomNumber);
+
 //   3.14,
+let numberPi = 3.14;
+console.log(numberPi);
+alert(numberPi);
+document.write(numberPi);
+
+
 //   2.7,
+
+let numberWithRemainder = 2.7;
+console.log(numberWithRemainder);
+alert(numberWithRemainder);
+document.write(numberWithRemainder);
+
 //   16,
+
+
+
 //   true,
 //   false
 //
