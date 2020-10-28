3c84c071e4b2ff8d3e9306e3a79e473a6013dabd
Serhiy <vintonyak0601@gmail.com>
2020-10-28 02:30:22 +0200

    JS lesadd dz1

--- JS/lesson 1/les1.js
+++ JS/lesson 1/les1.js
@@ -24,60 +24,61 @@
 // alert(codeUkraine);
 // document.write(codeUkraine);
 //   1,
-let number = 1;
-console.log(number);
-alert(number);
-document.write(number);
+// let number = 1;
+// console.log(number);
+// alert(number);
+// document.write(number);
 
 //   10,
-let ocWindows = 10;
-console.log(ocWindows);
-alert(ocWindows);
-document.write(ocWindows);
+// let ocWindows = 10;
+// console.log(ocWindows);
+// alert(ocWindows);
+// document.write(ocWindows);
 
 //   -999,
-let musicalGroup = -999;
-console.log(musicalGroup);
-alert(musicalGroup);
-document.write(musicalGroup);
+// let musicalGroup = -999;
+// console.log(musicalGroup);
+// alert(musicalGroup);
+// document.write(musicalGroup);
 
 
 //   123,
 
-let randomNumber = 123;
-console.log(randomNumber);
-alert(randomNumber);
-document.write(randomNumber);
+// let randomNumber = 123;
+// console.log(randomNumber);
+// alert(randomNumber);
+// document.write(randomNumber);
 
 //   3.14,
-let numberPi = 3.14;
-console.log(numberPi);
-alert(numberPi);
-document.write(numberPi);
+// let numberPi = 3.14;
+// console.log(numberPi);
+// alert(numberPi);
+// document.write(numberPi);
 
 
 //   2.7,
-
-let numberWithRemainder = 2.7;
-console.log(numberWithRemainder);
-alert(numberWithRemainder);
-document.write(numberWithRemainder);
+// let numberWithRemainder = 2.7;
+// console.log(numberWithRemainder);
+// alert(numberWithRemainder);
+// document.write(numberWithRemainder);
 
 //   16,
-
-let integer = 16;
-console.log(integer);
-alert(integer);
-document.write(integer);
+// let integer = 16;
+// console.log(integer);
+// alert(integer);
+// document.write(integer);
 
 //   true,
-
-let logicalOperator = true;
-console.log(logicalOperator);
-alert(logicalOperator);
-document.write(logicalOperator);
+// let logicalOperator = true;
+// console.log(logicalOperator);
+// alert(logicalOperator);
+// document.write(logicalOperator);
 
 //   false
+// let logicalOperator1 = false;
+// console.log(logicalOperator1);
+// alert(logicalOperator1);
+// document.write(logicalOperator1);
 //
 //
 // Вывести каждую при помощи console.log , alert, document.write
